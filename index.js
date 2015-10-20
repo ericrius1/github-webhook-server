c739d592a3b5d68345d93b13aa94cb8353fc2c0d
ericrius1 <ericrius1@gmail.com>
2015-10-20 13:36:32 -0700

    up to date

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ var app = express()
 var bodyParser = require('body-parser')
 app.use( bodyParser.json() ); 
 
+var latestBody = null;
 
 app.post('/payload', function(req, res) {
     console.log(req.body)
+    latestBody = req.body;
 });
 
 app.get('/github', function(req, res) {
-    console.log('hey!');
+    res.send(latestBody);
 });
 
 var server = app.listen(4567, function() {
